f2ab55faa03e4593b945a4a5da269635b6c15d00
cx9s <cx9918@gmail.com>
2023-03-07 13:50:14 +0000

    change to arrow function and and params' type

--- client/src/ui/console.ts
+++ client/src/ui/console.ts
@@ -1,30 +1,30 @@
 import * as readline from "node:readline/promises";
 import { EOL } from "os";
 
-export function printNewLine() {
+export const printNewLine = () => {
 	console.log(EOL); // this imports the correct End-Of-Line for either Windows or Unix
-}
+};
 
-export function print(str, newLine = true) {
+export const print = (str: string, newLine: boolean = true) => {
 	console.log(str);
 	if (newLine === true) {
 		printNewLine();
 	}
-}
+};
 
-export function clear(addTopBorder) {
+export const clear = (addTopBorder: string = "") => {
 	console.clear();
 	if (addTopBorder === "yes") {
 		print("------------------------------------");
 	}
-}
+};
 
 const reader = readline.createInterface({
 	input: process.stdin,
 	output: process.stdout,
 });
 
-export async function prompt(prompt) {
+export const prompt = async (prompt: string) => {
 	const answer = await reader.question(`${prompt}  > `);
 	return answer;
-}
+};
